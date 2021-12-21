c3cfb807ab28f4306035e3476a53355a017d1daa
royaljs <jiwonlee.tech@gmail.com>
2021-12-21 13:20:08 +0900

    Initialize Gradle project

---
+++ src/main/java/dev/jiwonlee/reactiveserver/ReactiveServerApplication.java
@@ -0,0 +1,13 @@
+package dev.jiwonlee.reactiveserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jiwonlee/reactiveserver/ReactiveServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jiwonlee.reactiveserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
