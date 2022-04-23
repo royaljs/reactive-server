3ff3df8446d52e6e4889a54865e7c5673f7c242d
royaljs <jiwonlee.tech@gmail.com>
2022-04-24 01:20:17 +0900

    Add auto Id generation of User entity

--- remoteserver/src/main/java/dev/jiwonlee/remoteserver/entity/User.java
+++ remoteserver/src/main/java/dev/jiwonlee/remoteserver/entity/User.java
@@ -1,6 +1,8 @@
 package dev.jiwonlee.remoteserver.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
@@ -14,6 +16,7 @@ import lombok.NoArgsConstructor;
 public class User {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String name;
